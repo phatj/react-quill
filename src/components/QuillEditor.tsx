bd14e698dc22cfbe8e07c3f28a3166f90cdbdbc0
Jamal Shah <jshah@fluxworkstudios.com>
2023-03-23 02:22:39 -0400

    feat: debounce output

--- src/components/QuillEditor.tsx
+++ src/components/QuillEditor.tsx
@@ -1,9 +1,23 @@
-import { FC, useState } from "react";
+import { FC, useEffect, useState } from "react";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
+import { useDebounce } from "react-use";
 
 export const QuillEditor: FC = () => {
   const [value, setValue] = useState("");
+  const [debouncedValue, setDebouncedValue] = useState("");
+
+  const [, cancel] = useDebounce(
+    () => {
+      setDebouncedValue(value);
+    },
+    450,
+    [value]
+  );
+
+  useEffect(() => {
+    console.log(debouncedValue);
+  }, [debouncedValue]);
 
   return <ReactQuill theme="snow" value={value} onChange={setValue} />;
 };
