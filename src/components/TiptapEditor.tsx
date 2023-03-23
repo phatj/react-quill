e881db1301cfb3ad881b47038d300b98386688ea
Jamal Shah <jshah@fluxworkstudios.com>
2023-03-23 16:36:23 -0400

    feat: add tiptap toolbar

--- src/components/TiptapEditor.tsx
+++ src/components/TiptapEditor.tsx
@@ -1,8 +1,30 @@
 import Link from "@tiptap/extension-link";
-import { EditorContent, useEditor } from "@tiptap/react";
+import { Editor, EditorContent, useEditor } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 import { FC } from "react";
 import { useEditorOutput } from "../hooks";
+
+type ToolbarProps = {
+  editor: Editor | null;
+};
+
+const Toolbar: FC<ToolbarProps> = ({ editor }) => {
+  if (!editor) {
+    return null;
+  }
+
+  return (
+    <div>
+      <button
+        onClick={() => editor.chain().focus().toggleBold().run()}
+        disabled={!editor.can().chain().focus().toggleBold().run()}
+        className={editor.isActive("bold") ? "is-active" : ""}
+      >
+        bold
+      </button>
+    </div>
+  );
+};
 
 export const TiptapEditor: FC = () => {
   const [value, setValue] = useEditorOutput("tiptap");
@@ -15,5 +37,10 @@ export const TiptapEditor: FC = () => {
     },
   });
 
-  return <EditorContent editor={editor} />;
+  return (
+    <>
+      <Toolbar editor={editor} />
+      <EditorContent editor={editor} />
+    </>
+  );
 };
